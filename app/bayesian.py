4143b6f7710099d1b9795314afd8f02704b437c1
calderkatyal <katyalcalder@gmail.com>
2024-03-14 15:41:20 -0400

    Attempted to use Docker to encapsulate env, will likely abandon

--- app/bayesian.py
+++ app/bayesian.py
@@ -1,2 +1,30 @@
-def perform_bayesian_analysis(data): 
-    return None
+import pymc3 as pm
+import numpy as np
+
+def perform_bayesian_analysis(data):
+    with pm.Model() as model:
+        # Using data provided in the dictionary
+        p_A = pm.Beta('p_A', alpha=data['alphaA'], beta=data['betaA'])
+        p_B = pm.Beta('p_B', alpha=data['alphaB'], beta=data['betaB'])
+
+        delta = pm.Deterministic('delta', p_A - p_B)
+
+        obs_A = pm.Binomial('obs_A', n=data['trialsA'], p=p_A, observed=data['successesA'])
+        obs_B = pm.Binomial('obs_B', n=data['trialsB'], p=p_B, observed=data['successesB'])
+
+        trace = pm.sample(1000, tune=500, step=pm.Metropolis(), return_inferencedata=False, progressbar=True)
+
+    p_A_samples = trace['p_A']
+    p_B_samples = trace['p_B']
+    delta_samples = trace['delta']
+
+    cred_interval = np.percentile(delta_samples, [2.5, 97.5])
+    prob = np.mean(delta_samples > 0)
+
+    return {
+        #'p_A_samples': p_A_samples.tolist(),
+        #'p_B_samples': p_B_samples.tolist(),
+        #'delta_samples': delta_samples.tolist(),
+        'cred_interval': cred_interval.tolist(),
+        'prob': prob
+    }
--- app/views.py
+++ app/views.py
@@ -1,12 +1,39 @@
 from flask import render_template, request, jsonify
-from app import app 
+from . import app  
 from .bayesian import perform_bayesian_analysis
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        data = request.form
+        # Extracting data from the form, make sure the indentation is correct
+        def to_float(value, default):
+            try:
+                return float(value)
+            except ValueError:
+                return default
+
+        # Extracting and converting data from the form
+        trialsA = int(request.form.get('trialsA', 0))
+        successesA = int(request.form.get('successesA', 0))
+        alphaA = to_float(request.form.get('alphaA', ''), 1.0)
+        betaA = to_float(request.form.get('betaA', ''), 1.0)
+        trialsB = int(request.form.get('trialsB', 0))
+        successesB = int(request.form.get('successesB', 0))
+        alphaB = to_float(request.form.get('alphaB', ''), 1.0)
+        betaB = to_float(request.form.get('betaB', ''), 1.0)
+
+        data = {
+            'trialsA': int(trialsA),
+            'successesA': int(successesA),
+            'alphaA': float(alphaA),
+            'betaA': float(betaA),
+            'trialsB': int(trialsB),
+            'successesB': int(successesB),
+            'alphaB': float(alphaB),
+            'betaB': float(betaB)
+        }
+
         results = perform_bayesian_analysis(data)
-        return jsonify(results)
+        return jsonify(results)  # or use render_template to show results
 
     return render_template('index.html')
